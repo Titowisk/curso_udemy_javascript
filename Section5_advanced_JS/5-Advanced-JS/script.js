4253c1a498e0916e6bf9a2d8814063c920c0cf11
Vitor Rabelo <rabelo51@gmail.com>
2018-06-13 14:41:36 -0300

    Class 57

--- Section5_advanced_JS/5-Advanced-JS/script.js
+++ Section5_advanced_JS/5-Advanced-JS/script.js
@@ -99,6 +99,7 @@ console.log(age);
 console.log(obj.city);
 */
 
+/*
 /////////////////////////////////////////////
 // Lecture: Passing functions as arguments (callback functions)
 
@@ -138,3 +139,33 @@ console.log(fullAges);
 
 rates = arrayCalc(ages, maxHeartRate);
 console.log(rates);
+*/
+
+/////////////////////////////////////////////
+// Lecture: Functions returning functions
+
+function interviewQuestion(job){
+    if ( job === 'designer'){
+        return function(name){
+            console.log(name + ' can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher'){
+        return function(name){
+            console.log('What subject do you teach, ' + name + ' ?');
+        }
+    } else {
+        return function(name){
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('Luke');
+
+// One can make a double call to call the functions
+interviewQuestion()('Robert');
+interviewQuestion('designer')('Katie');
