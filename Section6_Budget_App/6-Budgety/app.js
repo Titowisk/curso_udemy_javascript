1197b63380a84878ae64c98c6641f6969f6a8ef8
Vitor Rabelo <rabelo51@gmail.com>
2018-06-21 12:26:51 -0300

    Class 94

--- Section6_Budget_App/6-Budgety/app.js
+++ Section6_Budget_App/6-Budgety/app.js
@@ -265,6 +265,16 @@ var controller = (function (budgetCtrl, UICtrl){
         UICtrl.displayBudget(budget);
     };
 
+    var updatePercentages = function () {
+
+        // 1. Calculate the Percentages
+
+        // 2. Read percentages from budget controller
+
+        // 3. Update the UI with new percentages
+
+    };
+
     var ctrlAddItem = function(){
          var input, newItem;
 
@@ -284,6 +294,9 @@ var controller = (function (budgetCtrl, UICtrl){
 
             // 5. Calculate and Update budget
             updateBudget();
+
+            // 6. Calculate and update percentages
+            updatePercentages();
         }
     };
     
@@ -311,6 +324,9 @@ var controller = (function (budgetCtrl, UICtrl){
 
         // 3. update and show the new budget
         updateBudget();
+
+        // 4. Calculate and update percentages
+        updatePercentages();
 
     };
 
