94b0b999fc77a539e0b09c01a30990edf05b93f8
Vitor Rabelo <rabelo51@gmail.com>
2018-06-27 09:22:53 -0300

    Class 104

---
+++ Section7_ES6/starter/script.js
@@ -0,0 +1,58 @@
+// Lecture: let and const
+
+// ES5
+var name5 = 'Bob';
+name5 = 'Chad'; // it can change
+
+// ES6
+const name6 = 'Bob'; // It cannot be changed
+let age6 = '23'; // it can be changed.
+
+//var and let can vary but const cannot
+
+//// function scope x block scope
+// let and const works in block scope, var works in function scope
+
+// ES5
+function PrintTest5() {
+    console.log(variable + ' inside block scope'); // logs undefined because of hoisting
+
+    if (2 > 0){
+         var variable = 'variable';
+        console.log(variable + ' inside block scope');
+    }
+    console.log(variable + ' inside function scope');
+}
+
+PrintTest5();
+
+// ES6
+function PrintTest6() {
+    if (2 > 0){
+        let lettest = 'variable';
+        const constest = 'const test';
+        console.log(lettest + ' inside block scope');
+        console.log(constest + ' inside block scope');
+    }
+    // it wont log because the variables are inside the if block. (throws an error)
+    //console.log(lettest + ' inside block scope');
+    //console.log(constest + ' inside function scope');
+}
+
+PrintTest6();
+
+var i = 23;
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i); // logs 5 because variables are function scoped
+
+let j = 23;
+
+for(let j = 0; j < 5; j++) {
+    console.log(j);
+}
+
+console.log(j); // logs 23 because let is block scoped
