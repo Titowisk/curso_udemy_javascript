5d35eb80cd7c2569f8254d90f710fd5693638b51
Vitor Rabelo <rabelo51@gmail.com>
2018-07-09 11:12:54 -0300

    Class 134

--- Section9_Forkify/starter/webpack.config.js
+++ Section9_Forkify/starter/webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path') // built-in node module
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/js/index.js',
     output: {
         // https://nodejs.org/api/path.html#path_path_resolve_paths
         // https://nodejs.org/docs/latest/api/modules.html#modules_dirname
-        path: path.resolve(__dirname, 'dist/js'),
-        filename: 'bundle.js'
-    }
+        path: path.resolve(__dirname, 'dist/'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
 }
